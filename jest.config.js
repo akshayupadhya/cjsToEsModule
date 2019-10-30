25f39ae713d5d58eb269ced063ece1ba514c26ba
akshay upadhya <akshaysrinagar@gmail.com>
2019-10-30 17:57:13 +0530

    basic imort transform

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,23 @@
+const { defaults } = require('jest-config');
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
+  roots: ['<rootDir>'],
+  // modulePaths: ['<rootDir>/src'],
+  "moduleDirectories": [
+    "node_modules",
+    "<rootDir>/src",
+    "<rootDir>/test",
+     "src",
+    "test"
   ],
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
+  // bail: true,
+  // moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  // roots: ['src'],
+  // testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'],
+  // transform: {
+  //   '^.+\\.tsx?$': 'ts-jest',
+  // },
+  // verbose: true,
 }
