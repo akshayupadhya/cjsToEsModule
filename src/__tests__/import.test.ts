14d4d09612a65b5e7b3bb068ff5cc16e4f8da138
akshay upadhya <akshaysrinagar@gmail.com>
2019-10-31 11:14:44 +0530

    asyncReadFile in utils

--- src/__tests__/import.test.ts
+++ src/__tests__/import.test.ts
@@ -1,18 +1,14 @@
-import { readFile } from 'fs';
-import { promisify } from 'util';
 import { TransformImports } from '../import';
+import { asyncReadFile } from './utils'
 
-const asyncReadFile =  promisify(readFile);
 describe('import transform', () => {
-  it('file',async ()=>{
-    try{
+  it('file', async () => {
+    try {
       let file = await asyncReadFile('files/fileWithRequire.js', 'utf8')
       file = TransformImports(file)
       // console.info(file);
-    }catch(e){
+    } catch (e) {
       console.error(e)
     }
-
   })
-  // .then(console.info);
 });
